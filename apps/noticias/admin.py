adcb3405fcfba3e3da6304544f45eadaa02c1a86
FrancoSantaCruz <sczfranco@gmail.com>
2025-07-10 10:11:47 -0300

    [CLASE-19] Modelos en Django

--- apps/noticias/admin.py
+++ apps/noticias/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Noticia, Categoria
 
-# Register your models here.
+class NoticiaAdmin(admin.ModelAdmin):
+    fields = ('titulo', 'subtitulo', 'contenido', 'autor', 'categorias')
+
+    list_display = ('titulo', 'fecha', 'autor')
+
+    list_filter = ('fecha', 'autor', 'categorias')
+
+    search_fields = ('titulo', 'autor')
+
+
+admin.site.register(Noticia, NoticiaAdmin)
+admin.site.register(Categoria)
--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    categoria_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Noticia(models.Model):
+    noticia_id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=85)
+    subtitulo = models.CharField(max_length=150)
+    contenido = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    categorias = models.ManyToManyField(Categoria)
+    autor = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.titulo
