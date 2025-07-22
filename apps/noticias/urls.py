5a79f6d89654167ba7d8989b64a065e213b1fcb6
FrancoSantaCruz <sczfranco@gmail.com>
2025-07-22 15:46:17 -0300

    [Clase 21-B]Views CBV & Templates

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,21 +1,27 @@
 from django.urls import path
-# from .views import todas_las_noticias, una_noticia, noticia_categoria, crear_noticia, actualizar_noticia, eliminar_noticia
-
-from .views import TodasLasNoticiasView, UnaNoticiaView, CrearNoticiaView, ActualizarNoticiaView, EliminarNoticiaView
+from .views import (
+    TodasLasNoticiasView, 
+    UnaNoticiaView, 
+    CrearNoticiaView, 
+    ActualizarNoticiaView, 
+    EliminarNoticiaView,
+    todas_las_noticias,
+    una_noticia,
+    eliminar_noticia
+)
 
 #http://127.0.0.1:8000/noticias/
 
 urlpatterns = [
-    # path('', todas_las_noticias, name='todas_las_noticias'),
-    # path('detalle/', una_noticia, name='una_noticia'),
-    # path('categoria/', noticia_categoria, name='noticia_categoria'),
-    # path('crear/', crear_noticia, name='crear_noticia'),
-    # path('actualizar/', actualizar_noticia, name='actualizar_noticia'),
-    # path('eliminar/', eliminar_noticia, name='eliminar_noticia')
+    # CBV http://127.0.0.1:8000/noticias/cbv/
+    path('cbv/', TodasLasNoticiasView.as_view(), name="todas_las_noticias_cbv"),
+    path('cbv/<int:noticia_id>/', UnaNoticiaView.as_view(), name='una_noticia_cbv'),
+    path('cbv/crear/', CrearNoticiaView.as_view(), name='crear_noticia'),
+    path('cbv/actualizar/<int:noticia_id>/', ActualizarNoticiaView.as_view(), name='actualizar_noticia'),
+    path('cbv/eliminar/<int:noticia_id>/', EliminarNoticiaView.as_view(), name='eliminar_noticia_cbv'),
 
-    path('', TodasLasNoticiasView.as_view(), name="todas_las_noticias"),
-    path('<int:noticia_id>/', UnaNoticiaView.as_view(), name='una_noticia'),
-    path('crear/', CrearNoticiaView.as_view(), name='crear_noticia'),
-    path('actualizar/<int:noticia_id>/', ActualizarNoticiaView.as_view(), name='actualizar_noticia'),
-    path('eliminar/<int:noticia_id>/', EliminarNoticiaView.as_view(), name='eliminar_noticia')
+    # FBV http://127.0.0.1:8000/noticias/fbv/
+    path('fbv/', todas_las_noticias, name="todas_las_noticias_fbv"),
+    path('fbv/<int:noticia_id>/', una_noticia, name="una_noticia_fbv"),
+    path('fbv/eliminar/<int:noticia_id>/', eliminar_noticia, name="eliminar_noticia_fbv"),
 ]
--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -1,137 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Noticia, Autor, Categoria
 
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-'''
-    VISTAS BASADAS EN CLASES (CBV)
-'''
+
+# VISTA BASADA EN CLASES (CBV)
 class TodasLasNoticiasView(ListView):
     model = Noticia
-    template_name = 'todas_noticias.html'
-    context_object_name = 'noticias'
+    template_name = "todas_noticias.html"
+    context_object_name = "noticias"
 
-
-class UnaNoticiaView(DetailView):
-    model = Noticia
-    template_name = 'una_noticia.html'
-    context_object_name = 'noticia'
-    pk_url_kwarg = 'noticia_id'
-
-
-class CrearNoticiaView(CreateView):
-    model = Noticia
-    template_name = 'nueva_noticia.html'
-    fields = ['titulo', 'subtitulo', 'contenido']
-    success_url = reverse_lazy('todas_las_noticias')
-
-
-class ActualizarNoticiaView(UpdateView):
-    model = Noticia
-    template_name = 'actualizar_noticia.html'
-    fields = ['titulo', 'subtitulo']
-    success_url = reverse_lazy('todas_las_noticias')
-    pk_url_kwarg = 'noticia_id'
-
-
-class EliminarNoticiaView(DeleteView):
-    model = Noticia
-    template_name = 'eliminar_noticia.html'
-    success_url = reverse_lazy('todas_las_noticias')
-    pk_url_kwarg = 'noticia_id'
-
-'''
-    VISTAS BASADAS EN FUNCIONES (FBV)
-'''
-
+# VISTA BASADA EN FUNCIONES (FBV)
 def todas_las_noticias(request):
-    # BUSQUEDA DE INFORMACION GUARDADA
+    categoria_param = request.GET.get("categoria", "").strip()
+
+    # Buscar todas las noticias desde la base de datos.
     noticias = Noticia.objects.all()
 
+    if categoria_param:
+        noticias = noticias.filter(categorias__nombre__icontains=categoria_param)
 
-    #LOGICA REQUERIDA
+    # Meterlas en un contexto.
+    context = {"noticias": noticias, "categoria_seleccionada": categoria_param}
 
-
-    # CONSTRUCCION DE LA RESPUESTA
-    context = {
-        'noti': noticias
-    }
-
-    return render(request, 'todas_noticias.html', context)
-
-def una_noticia(request): 
-
-    noticia = Noticia.objects.get(noticia_id=3)
-
-    print(noticia.titulo)
-    print(noticia.subtitulo)
-    print(noticia.contenido)
-
-    return render(request, 'una_noticia.html')
-
-def noticia_categoria(request):
-
-    noticias_politica = Noticia.objects.filter(categorias__nombre="Politica")
-    print("Noticias en categoría 'Política':")
-    for noticia in noticias_politica:
-        print(f"    {noticia.titulo} - escrito por: {noticia.autor.nombre}")
-
-    return render(request, 'politica_noticias.html')
-
-# Create
-def crear_noticia(request):
-
-    cat = Categoria.objects.get(categoria_id=3)
-    autor = Autor.objects.get(autor_id=2)
-
-    nueva_noticia = Noticia(
-        titulo="Noticia en clase 1", 
-        subtitulo="Subtitulo en clase 1", 
-        contenido="Contenido en clase 1",
-        autor=autor
-        )
-    
-    nueva_noticia.save()
-    nueva_noticia.categorias.set([cat])
-
-    print("------------")
-    print(nueva_noticia.titulo)
-    print(nueva_noticia.subtitulo)
-    print(nueva_noticia.contenido)
-    print(nueva_noticia.autor.nombre)
-    print("------------")
-
-    return render(request, 'nueva_noticia.html')
-
-# Update
-def actualizar_noticia(request):
-
-    noticia_actual = Noticia.objects.filter(titulo="Noticia en clase 2 MODIFICADO").first()
-
-    noticia_actual.titulo = "Noticia en clase 3 MODIFICADOx2"
-    noticia_actual.subtitulo = "Subtitulo en clase 3 MODIFICADOx2"
-
-    noticia_actual.save()
-
-    print("------------")
-    print(noticia_actual.titulo)
-    print(noticia_actual.subtitulo)
-    print(noticia_actual.contenido)
-    print(noticia_actual.autor.nombre)
-    print("------------")
-
-    return render(request, 'actualizar_noticia.html')
-
-# Delete
-def eliminar_noticia(request):
-    noticia = Noticia.objects.get(noticia_id=14)
-    noticia.delete()
-
-    print("NOTICIA ELIMINADA CON EXITO")
-
-    return render(request, 'eliminar_noticia.html')
+    # Renderizar el html con el contexto.
+    return render(request, "todas_noticias.html", context)
 
 
 
+
+
+# VISTA BASADA EN CLASES (CBV)
+class UnaNoticiaView(DetailView):
+    model = Noticia
+    template_name = "una_noticia.html"
+    context_object_name = "noticia"
+    pk_url_kwarg = "noticia_id"
+
+# VISTA BASADA EN FUNCIONES (FBV)
+def una_noticia(request, noticia_id):
+    noticia = Noticia.objects.get(noticia_id=noticia_id)
+    context = {"noticia": noticia}
+    return render(request, "una_noticia.html", context)
+
+
+
+
+
+# VISTA BASADA EN CLASES (CBV)
+class CrearNoticiaView(CreateView):
+    model = Noticia
+    template_name = "nueva_noticia.html"
+    fields = ["titulo", "subtitulo", "contenido"]
+    success_url = reverse_lazy("todas_las_noticias")
+
+# VISTA BASADA EN FUNCIONES (FBV)
+class ActualizarNoticiaView(UpdateView):
+    model = Noticia
+    template_name = "actualizar_noticia.html"
+    fields = ["titulo", "subtitulo"]
+    success_url = reverse_lazy("todas_las_noticias")
+    pk_url_kwarg = "noticia_id"
+
+
+
+
+
+# VISTA BASADA EN CLASES (CBV)
+class EliminarNoticiaView(DeleteView):
+    model = Noticia
+    template_name = "eliminar_noticia.html"
+    success_url = reverse_lazy("todas_las_noticias")
+    pk_url_kwarg = "noticia_id"
+
+# VISTA BASADA EN FUNCIONES (FBV)
+def eliminar_noticia(request, noticia_id):
+    noticia = Noticia.objects.get(noticia_id=noticia_id)
+
+    if request.method == "POST":
+        noticia.delete()
+        return redirect("todas_las_noticias_fbv")
+
+    return render(request, "eliminar_noticia.html", {"noticia": noticia})
